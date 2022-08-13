6f210c8d1998a23fd42b67ed15f5a921b6a9daac
Agustin Zamar <agustinzamar33@gmail.com>
2022-08-12 21:55:29 -0300

    fix full article view

--- src/pages/guest/FullArticle.jsx
+++ src/pages/guest/FullArticle.jsx
@@ -5,7 +5,7 @@ import { useGetArticle } from '../../hooks/articles/useGetArticle'
 
 export const FullArticle = () => {
     const { id } = useParams()
-    const article = useGetArticle(id)
+    const [article] = useGetArticle(id)
 
     if (!article) {
         return <h1>Not Found</h1>
