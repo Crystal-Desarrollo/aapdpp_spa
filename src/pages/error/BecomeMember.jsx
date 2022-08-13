494afa142b416151b2b038a428929abb98b19e97
Franco <franco.p.0109@gmail.com>
2022-08-13 14:54:39 -0300

    member screen

--- src/pages/error/BecomeMember.jsx
+++ src/pages/error/BecomeMember.jsx
@@ -1,3 +1,34 @@
+import styled from 'styled-components'
+import { BecomeMemberImage } from '../../asssets/img/images'
+
+const Section = styled.section`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 3rem;
+`
+
+const Title = styled.h1`
+    font-size: 3rem;
+    text-align: center;
+`
+
+const ImageContainer = styled.div`
+    width: 90%;
+    max-width: 280px;
+`
+
 export const BecomeMember = () => {
-    return <h1>Hazte Miembro para acceder</h1>
+    return (
+        <Section>
+            <Title>
+                Hazte Miembro <br /> para acceder
+            </Title>
+            <ImageContainer>
+                <BecomeMemberImage />
+            </ImageContainer>
+        </Section>
+    )
 }
