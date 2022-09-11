542262ee12b8b83f6ac206941b06e9b028fa6295
Agustin Zamar <agustin.zamar@devsu.com>
2022-09-11 19:03:08 -0300

    add 404 page if user not found

--- src/pages/member/Profile.jsx
+++ src/pages/member/Profile.jsx
@@ -3,9 +3,14 @@ import { PaymentInformation } from '../../components/Profile/PaymentInformation'
 import { useParams } from 'react-router-dom'
 import { useGetUser } from '../../hooks/users/useGetUser'
 import { Section } from '../../components/Common/Section'
+import { NotFound } from '../error/NotFound'
 export const Profile = () => {
     const { id } = useParams()
     const user = useGetUser(id)
+
+    if (!user) {
+        return <NotFound />
+    }
 
     return (
         <Section>
