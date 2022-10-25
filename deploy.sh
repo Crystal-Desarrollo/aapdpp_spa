fbcc673301f0741ec8d6b8e560ca327a00e5fb5a
Agustin Zamar <agustinzamar33@gmail.com>
2022-10-25 00:53:10 -0300

    improve deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-npm run build && cp -a ./build/. .
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" \
+&& nvm use --lts \
+&& npm run build \
+&& cp -a ./build/. .
