2146e835d61bf273c06ec2c229a7d6eccc38d11d
Agustin Zamar <agustinzamar33@gmail.com>
2023-06-29 18:59:24 -0300

    update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash \
+git pull origin master \
+&& wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash \
 && export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" \
