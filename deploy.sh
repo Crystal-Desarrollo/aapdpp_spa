1371289af8803955ebfed6048efdb2e8ccd93e1a
Agustin Zamar <agustinzamar33@gmail.com>
2023-07-27 13:03:44 -0300

    improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-git pull origin master \
-&& wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash \
+git pull \
 && export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" \
-&& nvm install 16.10.0 \
+&& nvm install 18 \
+&& nvm use 18 \
 && npm install \
 && npm run build \
 && cp -a ./build/. . \
 && rm -rf ./build \
 && rm -rf ./node_modules \
 && cd aapdpp-api \
-&& git pull origin master \
+&& git pull \
 && php ~/composer.phar install \
-&& php artisan migrate --force
+&& php artisan migrate --force \
+&& php artisan optimize \
+&& php artisan event:cache
