d6a6813b0a5bfab590b1c2829984fdb40eb12e3e
Agustin Zamar <agustinzamar33@gmail.com>
2025-05-12 18:35:56 -0300

    feat: show inscriptions

--- src/components/Inscriptions/inscriptions.jsx
+++ src/components/Inscriptions/inscriptions.jsx
@@ -12,25 +12,25 @@ function Inscriptions() {
                     <p>
                         20° Encuentro nacional de profesores de derecho procesal penal <br /> 18 y 19 de Septiembre
                     </p>
-                    <div className="buttons">
-                        <a
-                            rel='noreferrer'
-                            className="btnSecundario"
-                            href={documento}
-                            target="_blank"
-                        >
-                            Cronograma
-                        </a>
-                        <a
-                            rel='noreferrer'
-                            className="btnPrimario"
-                            href="https://forms.gle/F11o1oLFwxVrXQ3s8"
-                            target="_blank"
-                        >
-                            {' '}
-                            ¡Inscribirme!
-                        </a>
-                    </div>
+                    {/*<div className="buttons">*/}
+                    {/*    <a*/}
+                    {/*        rel='noreferrer'*/}
+                    {/*        className="btnSecundario"*/}
+                    {/*        href={documento}*/}
+                    {/*        target="_blank"*/}
+                    {/*    >*/}
+                    {/*        Cronograma*/}
+                    {/*    </a>*/}
+                    {/*    <a*/}
+                    {/*        rel='noreferrer'*/}
+                    {/*        className="btnPrimario"*/}
+                    {/*        href="https://forms.gle/F11o1oLFwxVrXQ3s8"*/}
+                    {/*        target="_blank"*/}
+                    {/*    >*/}
+                    {/*        {' '}*/}
+                    {/*        ¡Inscribirme!*/}
+                    {/*    </a>*/}
+                    {/*</div>*/}
                 </div>
                 <div className="img">
                     <img src={imagenInscripcion} alt="" />
